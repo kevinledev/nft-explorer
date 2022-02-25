d4bdbdb81b924358647872da827c377328df5f08
kevinledev <kevinle.developer@gmail.com>
2022-02-25 01:21:59 -0800

    Adding setup files

---
+++ src/generator/raritycalc.js
@@ -0,0 +1,6 @@
+const Moralis = require('moralis/node');
+require('dotenv').config({ path: '/Users/kevinle/Projects/nft-explorer/.env'});
+
+const serverUrl = process.env.SERVER_URL;
+const appId = process.env.APP_ID;
+
