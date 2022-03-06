c42035e783201fcd0a93d675a1cb7ad6aa75614c
kevinledev <kevinle.developer@gmail.com>
2022-03-05 16:16:30 -0800

    fixed navbar styling

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const config = {
   entry: [
-    path.resolve(__dirname, "src", "index.js"),
-    path.resolve(__dirname, "src", "index.scss"),
+    path.resolve(__dirname, "src", "index.js")
+    // path.resolve(__dirname, "src", "index.scss"),
   ],
   output: {
     path: path.join(__dirname, "dist"), // bundled file in dist/
